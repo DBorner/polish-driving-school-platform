9b81baf86a3e28ef8b894e1cdcc295039f5847fd
Daniel Borner <megadan176@gmail.com>
2023-05-27 17:27:59 +0200

    secured adding student

--- oskplatform/course/views.py
+++ oskplatform/course/views.py
@@ -5,6 +5,7 @@ from password_generator import PasswordGenerator
 from users.models import CustomUser, Student
 from course.models import PracticalLesson, Course
 from django.utils import timezone
+from course.forms import NewStudentForm
 
 @login_required(login_url='/login')
 def panel_view(request):
@@ -31,22 +32,26 @@ def register_student_view(request):
     if request.user.permissions_type != 'E':
         return HttpResponse('Nie masz uprawnień do tej strony')
     if request.method == 'POST':
-        surname = request.POST.get('surname')
-        name = request.POST.get('name')
-        birth_date = request.POST.get('birth-date')
-        phone_number = request.POST.get('phone-number')
-        email = request.POST.get('email')
-        id = CustomUser.objects.order_by('-pk')[0].pk + 1
-        username = f'{surname.lower()[:3]}{name.lower()[:3]}{id}'
-        pwo = PasswordGenerator()
-        pwo.minlen = 6
-        pwo.maxlen = 6
-        password = pwo.generate()
-        print(password)
-        student = Student.objects.create(surname=surname, name=name, birth_date=birth_date, phone_number=phone_number, email=email)
-        user = CustomUser.objects.create_user(username=username, password=password, permissions_type='S', student=student)
-        student.save()
-        user.save()
+        form = NewStudentForm(request.POST)
+        if form.is_valid():
+            surname = request.POST.get('surname')
+            name = request.POST.get('name')
+            birth_date = request.POST.get('birth_date')
+            phone_number = request.POST.get('phone_number')
+            email = request.POST.get('email')
+            id = CustomUser.objects.order_by('-pk')[0].pk + 1
+            username = f'{surname.lower()[:3]}{name.lower()[:3]}{id}'
+            pwo = PasswordGenerator()
+            pwo.minlen = 6
+            pwo.maxlen = 6
+            password = pwo.generate()
+            print(password)
+            student = Student.objects.create(surname=surname, name=name, birth_date=birth_date, phone_number=phone_number, email=email)
+            user = CustomUser.objects.create_user(username=username, password=password, permissions_type='S', student=student)
+            student.save()
+            user.save()
+        else:
+            return HttpResponse('Niepoprawne dane')
         return HttpResponse(f'Utworzono nowego kursanta {student} o nazwie użytkownika {user.username} i haśle {password}')
     template = loader.get_template('register_student.html')
     return HttpResponse(template.render({}, request))
