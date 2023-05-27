9aaf8761d74674d2dd7bbacb402e92e6e8440b3e
Daniel Borner <megadan176@gmail.com>
2023-05-27 13:35:39 +0200

    added panel view

--- oskplatform/course/views.py
+++ oskplatform/course/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+@login_required(login_url='/login')
+def panel_view(request):
+    template = loader.get_template('panel.html')
+    return HttpResponse(template.render({}, request))
--- oskplatform/oskplatform/urls.py
+++ oskplatform/oskplatform/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from users import views as user_views
 from platformsite import views as platform_views
+from course import views as course_views
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -14,5 +15,6 @@ urlpatterns = [
     path('theorys/', platform_views.theorys),
     path('login/', user_views.login_view),
     path('logout/', user_views.logout_view),
+    path('panel/', course_views.panel_view),
     path('', platform_views.home),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
