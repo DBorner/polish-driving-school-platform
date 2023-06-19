2700684d0ac2d3ab9129577243bb0562de48e98c
Daniel Borner <megadan176@gmail.com>
2023-06-19 18:48:40 +0200

    Added practical lessons gen

--- oskplatform/createFakeData.py
+++ oskplatform/createFakeData.py
@@ -1,5 +1,5 @@
 import os 
-from tqdm import trange
+from tqdm import trange, tqdm
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oskplatform.settings")
 
 import django 
@@ -7,6 +7,7 @@ django.setup()
 
 from faker import Faker 
 import random
+from datetime import date
 from course.models import Category, Course, PracticalLesson 
 from users.models import Instructor, Student, Qualification
 from model_bakery import baker
@@ -56,7 +57,27 @@ def generate_students(how_many=10, finished_courses=True):
                 course.course_status = 'Z'
             course.save()
     print("Done!")
-
+    
+def generate_practical_lessons():
+    fake = Faker(['pl_PL'])
+    for course in tqdm(Course.objects.filter(course_status='R'), desc="Progress: "):
+        instructor = pick_instructor(course.category)
+        num_of_hours = 0
+        for lesson in PracticalLesson.objects.filter(course=course):
+            num_of_hours += lesson.num_of_hours
+        if num_of_hours < course.category.required_practical_hours:
+            hours = random.randint(0, course.category.required_practical_hours - num_of_hours)
+            while num_of_hours < hours:
+                practical_lesson = baker.make(PracticalLesson,
+                                        date=fake.date_between(start_date='+1m', end_date='+2m'),
+                                        start_time=fake.time(),
+                                        num_of_hours=random.randint(1, 2),
+                                        num_of_km=random.randint(10, 50),
+                                        cost=None,
+                                        instructor=instructor,
+                                        course=course
+                )
+                num_of_hours += practical_lesson.num_of_hours
 
 def pick_instructor(category):
     qualifications = Qualification.objects.filter(category=category)
@@ -69,18 +90,21 @@ def start_script():
         input("It will only work if you have already created instructors, categories and qualifications. Press ENTER to continue...")
         x = input("Are you sure you want to continue? (y/n): ")
         if x == 'y':
-            if input("Do you want to create finished courses? (y/n): ") == 'y':
-                finished_courses = True
+            if input('1 - Generate students 2 - Generate practical lessons: ') == '1':
+                if input("Do you want to create finished courses? (y/n): ") == 'y':
+                    finished_courses = True
+                else:
+                    finished_courses = False
+                try:
+                    how_many = int(input("How many students do you want to create? (default: 10): "))
+                except ValueError:
+                    print("Wrong input. Default value will be used.")
+                    generate_students(finished_courses=finished_courses)
+                if how_many <= 0:
+                    generate_students(finished_courses=finished_courses)
+                generate_students(how_many, finished_courses=finished_courses)
             else:
-                finished_courses = False
-            try:
-                how_many = int(input("How many students do you want to create? (default: 10): "))
-            except ValueError:
-                print("Wrong input. Default value will be used.")
-                generate_students(finished_courses=finished_courses)
-            if how_many <= 0:
-                generate_students(finished_courses=finished_courses)
-            generate_students(how_many, finished_courses=finished_courses)
+                generate_practical_lessons()
     except KeyboardInterrupt:
         print("\nAborted.")
         
